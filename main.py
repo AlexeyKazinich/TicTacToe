f8be5867d5eeb0178805f6b9eef228017244a1e3
AlexeyKazinich <alexey.kazinich@gmail.com>
2023-08-21 13:36:05 -0400

    refactored code

--- main.py
+++ main.py
@@ -40,46 +40,54 @@ class TicTacToe:
             pygame.draw.line(self.screen,(0,0,0),(0,self.screen.get_height() / 3 * i),(self.screen.get_width(),self.screen.get_height() / 3 * i))
             
 
+    def logic(self) -> None:
+        """perform all the logic for the game loop"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.paused = not self.paused
+                    print(self.paused)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if not self.paused:
+                    for key, value in self.grid.items():
+                        if value.rect.collidepoint(pygame.mouse.get_pos()):
+                            value.color = (0,255,0)
+                            value.text = self.player
+                            if self.player == "X":
+                                self.player = "O"
+                            elif self.player == "O":
+                                self.player = "X"
+                
+                #if game is paused                
+                elif self.paused:
+                    pass 
+    
+    def draw(self) -> None:
+        """draw all the things"""
+        
+        self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()),(0,0))
+        self.draw_grid()
+        
+        if self.paused: 
+            #cover screen with a bit of fog
+            pause_color = pygame.Surface((640,480),pygame.SRCALPHA)
+            pause_color.fill((255,255,255, 150))
+            self.screen.blit(pause_color,(0,0))
+            #draw buttons
+        
     def run(self) -> None:
         '''main gameloop, call this to run the game'''
         while True:
             self.display.fill((0,0,0))
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        self.paused = not self.paused
-                        print(self.paused)
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if not self.paused:
-                        for key, value in self.grid.items():
-                            if value.rect.collidepoint(pygame.mouse.get_pos()):
-                                value.color = (0,255,0)
-                                value.text = self.player
-                                if self.player == "X":
-                                    self.player = "O"
-                                elif self.player == "O":
-                                    self.player = "X"
-                    
-                    #if game is paused                
-                    elif self.paused:
-                        pass 
+            self.logic()
                     
             
-            
-            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()),(0,0))
-            self.draw_grid()
-            if self.paused:
-                #cover screen with a bit of fog
-                pause_color = pygame.Surface((640,480),pygame.SRCALPHA)
-                pause_color.fill((255,255,255, 150))
-                self.screen.blit(pause_color,(0,0))
-                #draw buttons
-                
+            self.draw()    
             pygame.display.update()
             self.clock.tick(60)
     
